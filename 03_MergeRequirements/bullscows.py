c1e7f2908a4b495f0b892624fc8530f6beae3dd5
EvgSumina <eugenia.sumina@yandex.ru>
2023-03-05 12:21:02 +0300

    Add reading word_arr in bullscows

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -1,4 +1,6 @@
 from argparse import ArgumentParser
+import os
+from urllib.request import urlretrieve
 
 
 if __name__ == "__main__":
@@ -7,3 +9,17 @@ if __name__ == "__main__":
     parser.add_argument('length', type=int, action='store', nargs='?', default=5,
                         help='length of used words')
     args = parser.parse_args()
+
+    filename = args.dictionary
+    if not os.path.exists(filename):
+        filename, _ = urlretrieve(args.dictionary)
+
+    word_arr = []
+    with open(filename, 'r') as f:
+        for line in f.readlines():
+            line = line.strip()
+            if len(line) == args.length:
+                word_arr.append(line)
+
+    if len(word_arr) == 0:
+        print('There are no words with needed length')
